bb8295c13d7996f27ad25c13b08eda1143fc79c2
original-pokemon <eremkin-02@mail.ru>
2021-03-12 17:45:00 +0300

    feat: add towel-sort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) return [];
+
+  let result = [];
+  
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+
+      for (let j = 0; j < matrix[i].length; j++) {
+        result.push(matrix[i][j])
+      }
+    } else {
+
+      for (let l = matrix[i].length - 1; l >= 0; l--) {
+        result.push(matrix[i][l])
+      }
+    }
+  }
+  return result;
 }
